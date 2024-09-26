872972dc9379b669e3f4918357bcb615f57506d2
sumantacharya <sumanthacharya399@gmail.com>
2024-09-26 22:39:31 +0530

    variable and datatype in java

---
+++ VariableSession.java
@@ -0,0 +1,24 @@
+public class VariableSession{
+	public static void main(String[] args){
+		byte numberOfSeatsInCar=4;
+		byte listOfFruitsInBowl=6;
+		byte noOfAlphabets=26;
+		byte minimumGfInMonth=7;
+		byte noOfBooksInBag=5;
+		byte noOfPencilsInPouch=3;
+		byte maximunStudentInClass=80;
+		byte minimumLecturesInMcaDepartment=5;
+		short noOfStudentsInSchool=2000;
+		short maximumSpeedOfVechicle=200;
+		short storesTheScoreOfPlayerInGame=500;
+		short storesMeasuredDistance=500;
+		short representWeightOfThePerson=200;
+		short numberOfPassengerInTrain=3000;
+		short yearBirth=1986;
+	    short DurationOfEventsInMinutes = 120;
+	    System.out.println(numberOfSeatsInCar);
+	    numberOfSeatsInCar=8;
+	    System.out.println(numberOfSeatsInCar);
+		
+	}
+}
