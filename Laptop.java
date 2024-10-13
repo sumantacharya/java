d1e4096d53412393c990274830a86ea69c5fdc65
sumantacharya <sumanthacharya399@gmail.com>
2024-10-13 17:47:35 +0530

    example for object

---
+++ Laptop.java
@@ -0,0 +1,22 @@
+public class Laptop{
+	String lapName;
+    String modelName;
+	String display;
+	String procesor;
+	int ram;
+  public static void shutDown(){
+
+  }
+  public static void openApplication(){
+
+  }
+  public static void update(){
+
+  }
+  public static void closeApplication(){
+
+  }
+  public static void connectWifi(){
+  	
+  }
+}
