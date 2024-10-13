d5af74590d9a17926d1177ec8a29e6919d1dbd8e
sumantacharya <sumanthacharya399@gmail.com>
2024-10-13 17:45:21 +0530

    object creating

---
+++ LaptopInfo.java
@@ -0,0 +1,146 @@
+public class LaptopInfo{
+	public static void main(String[] args){
+		Laptop laptop=new Laptop();
+		laptop.lapName="samsang";
+		laptop.modelName="LDR465";
+		laptop.display="LED";
+		laptop.procesor="RYZON5";
+		laptop.ram=8;
+
+		System.out.println("lap name:" +laptop.lapName);
+		System.out.println("lap model name:" +laptop.modelName);
+		System.out.println("display:" +laptop.display);
+		System.out.println("procesor:" +laptop.procesor);
+		System.out.println("ram:" +laptop.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop2=new Laptop();
+		laptop2.lapName="lenovo";
+		laptop2.modelName="LDR4k5";
+		laptop2.display="LCD";
+		laptop2.procesor="RYZON3";
+		laptop2.ram=8;
+
+		System.out.println("lap name:" +laptop2.lapName);
+		System.out.println("lap model name:" +laptop2.modelName);
+		System.out.println("display:" +laptop2.display);
+		System.out.println("procesor:" +laptop2.procesor);
+		System.out.println("ram:" +laptop2.ram);
+	    System.out.println("-----------------------------------");
+
+
+		Laptop laptop3 =new Laptop();
+		laptop3.lapName="DELL";
+		laptop3.modelName="DDR465";
+		laptop3.display="QLCD";
+		laptop3.procesor="intel_3";
+		laptop3.ram=4;
+
+		System.out.println("lap name:" +laptop3.lapName);
+		System.out.println("lap model name:" +laptop3.modelName);
+		System.out.println("display:" +laptop3.display);
+		System.out.println("procesor:" +laptop3.procesor);
+		System.out.println("ram:" +laptop3.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop4 =new Laptop();
+		laptop4.lapName="acer";
+		laptop4.modelName="DDR485";
+		laptop4.display="QLED";
+		laptop4.procesor="intel_5";
+		laptop4.ram=8;
+
+		System.out.println("lap name:" +laptop4.lapName);
+		System.out.println("lap model name:" +laptop4.modelName);
+		System.out.println("display:" +laptop4.display);
+		System.out.println("procesor:" +laptop4.procesor);
+		System.out.println("ram:" +laptop4.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop5 =new Laptop();
+		laptop5.lapName="MAC";
+		laptop5.modelName="HWVD64";
+		laptop5.display="LCD";
+		laptop5.procesor="intel_8";
+		laptop5.ram=10;
+
+		System.out.println("lap name:" +laptop5.lapName);
+		System.out.println("lap model name:" +laptop5.modelName);
+		System.out.println("display:" +laptop5.display);
+		System.out.println("procesor:" +laptop5.procesor);
+		System.out.println("ram:" +laptop5.ram);
+		System.out.println("-----------------------------------");
+
+
+		Laptop laptop6 =new Laptop();
+		laptop6.lapName="REDMI";
+		laptop6.modelName="HWSHG25";
+		laptop6.display="LCD";
+		laptop6.procesor="intel_5";
+		laptop6.ram=8;
+
+		System.out.println("lap name:" +laptop6.lapName);
+		System.out.println("lap model name:" +laptop6.modelName);
+		System.out.println("display:" +laptop6.display);
+		System.out.println("procesor:" +laptop6.procesor);
+		System.out.println("ram:" +laptop6.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop7 =new Laptop();
+		laptop7.lapName="ASUS";
+		laptop7.modelName="KHF56";
+		laptop7.display="QLCD";
+		laptop7.procesor="intel_5";
+		laptop7.ram=8;
+
+		System.out.println("lap name:" +laptop7.lapName);
+		System.out.println("lap model name:" +laptop7.modelName);
+		System.out.println("display:" +laptop7.display);
+		System.out.println("procesor:" +laptop7.procesor);
+		System.out.println("ram:" +laptop7.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop8 =new Laptop();
+		laptop8.lapName="HP";
+		laptop8.modelName="HWSH85";
+		laptop8.display="LCD";
+		laptop8.procesor="intel_3";
+		laptop8.ram=4;
+
+		System.out.println("lap name:" +laptop8.lapName);
+		System.out.println("lap model name:" +laptop8.modelName);
+		System.out.println("display:" +laptop8.display);
+		System.out.println("procesor:" +laptop8.procesor);
+		System.out.println("ram:" +laptop8.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop9 =new Laptop();
+		laptop9.lapName="REALME";
+		laptop9.modelName="HWSHG254";
+		laptop9.display="LCD";
+		laptop9.procesor="intel_7";
+		laptop9.ram=8;
+
+		System.out.println("lap name:" +laptop9.lapName);
+		System.out.println("lap model name:" +laptop9.modelName);
+		System.out.println("display:" +laptop9.display);
+		System.out.println("procesor:" +laptop9.procesor);
+		System.out.println("ram:" +laptop9.ram);
+		System.out.println("-----------------------------------");
+
+		Laptop laptop10 =new Laptop();
+		laptop10.lapName="ONIDA";
+		laptop10.modelName="SHG256";
+		laptop10.display="LQLED";
+		laptop10.procesor="intel_7";
+		laptop10.ram=8;
+
+		System.out.println("lap name:" +laptop10.lapName);
+		System.out.println("lap model name:" +laptop10.modelName);
+		System.out.println("display:" +laptop10.display);
+		System.out.println("procesor:" +laptop10.procesor);
+		System.out.println("ram:" +laptop10.ram);
+		System.out.println("-----------------------------------");
+
+	}
+}
